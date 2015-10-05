afdc5ec479669ab70f38049ae8a4fcfcbedbfb89
luoyy <1070753575@qq.com>
2015-10-05 20:33:17 +0800

    add Bmob

--- app/src/main/java/team/far/footing/app/APP.java
+++ app/src/main/java/team/far/footing/app/APP.java
@@ -3,6 +3,9 @@ package team.far.footing.app;
 import android.app.Application;
 import android.content.Context;
 
+import cn.bmob.v3.Bmob;
+import team.far.footing.R;
+
 /**
  *
  * Created by moi on 15/10/2.
@@ -10,12 +13,13 @@ import android.content.Context;
 public class APP extends Application {
 
     private static Context context;
-
     @Override
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
         // 各种初始化
+        // 初始化 Bmob SDK
+        Bmob.initialize(this, context.getString(R.string.Bmob_Key));
     }
 
     public static Context getContext() {
