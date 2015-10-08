a177338696abc759da17eb698b52f55122826b8e
luoyy <1070753575@qq.com>
2015-10-08 19:52:52 +0800

    增加注释

--- app/src/main/java/team/far/footing/model/IUserModel.java
+++ app/src/main/java/team/far/footing/model/IUserModel.java
@@ -7,10 +7,27 @@ import team.far.footing.model.callback.OnUserListener;
  * Created by moi on 15/10/2.
  */
 public interface IUserModel {
-    void loginGetCode(String phonenumber,OnLoginListener onLoginListener);
+    /**
+     * 发起请求获取验证码
+     *
+     * @param phonenumber     -----手机号
+     * @param onLoginListener -----获取与验证的监听器
+     */
+    void loginGetCode(String phonenumber, OnLoginListener onLoginListener);
 
-    void loginVerifyCode(String phonenumber,String code);
+    /**
+     * 使用验证码验证
+     *
+     * @param phonenumber ----手机号
+     * @param code        ----验证码
+     */
+    void loginVerifyCode(String phonenumber, String code);
 
+    /**
+     * 注册的监听器  --> 注册的gui包含在这里面，所以调用此方法就会弹出注册的gui
+     *
+     * @param onUserListener
+     */
     void register(OnUserListener onUserListener);
 
     void logout(OnUserListener onUserListener);
